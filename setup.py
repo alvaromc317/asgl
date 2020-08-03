ed06fe28045895be1aea3843857f8f6eab82aa42
Álvaro Méndez Civieta <alvaromc317@hotmail.com>
2020-08-03 16:07:48 +0200

    small change

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_packages
 
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='asgl',
     version='v0.0.1',
@@ -15,7 +9,6 @@ setup(
     zip_safe=False,
     url='https://github.com/alvaromc317/ASGL',
     description='A regression solver for linear and quantile regression models and lasso based penalizations',
-    long_description=long_description,
     python_requires='>=3.5',
     install_requires=["cvxpy >= 1.1.0",
                       "numpy >= 1.15",
